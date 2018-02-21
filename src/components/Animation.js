30e6857464661c2b378ffb9711e15afc92f72ea6
Aimee McGrenera <aimee.mcgrenera@gmail.com>
2018-02-21 21:47:29 +0000

    automatically backed up by learn

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -31,7 +31,7 @@ class Animation extends React.Component {
     setTimeout(() => (progressBar.className = "off"), 1100);
   };
 
-  componentWillUpdate(){
+  componentWillUpdate(nextProps, nextState){
     if (nextState.open == true && this.state.open == false) {
     this.props.showLoadingBar();
   }
